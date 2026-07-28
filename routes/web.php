1b1d0bab9360fbe1590d58246cfdc7874fac13de
umifatih <234110601047@mhs.uinsaizu.ac.id>
2026-07-29 02:15:37 +0700

    folder

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::view('/', 'pages.home.index')->name('home');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/profil', 'pages.profile.index')->name('profil');
+
+Route::view('/warga-sekolah', 'pages.warga.index')->name('warga');
+
+Route::view('/akademik', 'pages.akademik.index')->name('akademik');
+
+Route::view('/berita', 'pages.berita.index')->name('berita');
+
+Route::view('/galeri', 'pages.galeri.index')->name('galeri');
+
+Route::view('/informasi', 'pages.informasi.index')->name('informasi');
+
+Route::view('/ppdb', 'pages.ppdb.index')->name('ppdb');
+
+Route::view('/kontak', 'pages.kontak.index')->name('kontak');
